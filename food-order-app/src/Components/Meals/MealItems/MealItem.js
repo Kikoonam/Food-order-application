5780c0445def0f66973c7bc297e94a2e5550ce53
Hongduzi Saiershanbayi <92166985+Kikoonam@users.noreply.github.com>
2023-04-20 10:44:33 +0200

    Adding individual Meal Items & Displying Them

---
+++ food-order-app/src/Components/Meals/MealItems/MealItem.js
@@ -0,0 +1,17 @@
+import classes from './MealItem.module.css'
+
+export default function MealItem (props){
+    const Price = `$${props.price.toFixed(2)}`
+     
+    return (
+    <li className={classes.meal}> 
+    <div>
+        <h3 className={classes.name}>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{Price}</div>
+    </div>
+    <div>
+
+    </div>
+    </li>);
+}
